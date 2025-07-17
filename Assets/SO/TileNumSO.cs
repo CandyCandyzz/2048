b57d913d3f61eba7677b16a29bef5d090804c22d
CandyCandyzz <dieubodoi@gmail.com>
2025-07-17 23:36:55 +0700

    project

--- Assets/SO/TileNumSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TileNumSO", menuName = "Scriptable Objects/TileNumSO")]
-public class TileNumSO : ScriptableObject
-{
-    public int value;
-    public Color colorTile;
-    public Color colorNum;
-}
